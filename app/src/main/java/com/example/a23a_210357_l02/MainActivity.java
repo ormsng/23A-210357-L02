4afa7061fe562870a2cdca80df044c4a208a7b20
Or Messing <ormessing1@gmail.com>
2022-11-03 11:27:23 +0200

    Init project

---
+++ app/src/main/java/com/example/a23a_210357_l02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a23a_210357_l02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
